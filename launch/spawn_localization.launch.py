5495f5ee8dd41486e2165bd23d81d6b729755581
Hercogs <jecuks96@gmail.coms>
2023-11-30 13:31:42 +0200

    Initial commit

---
+++ launch/spawn_localization.launch.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import TextSubstitution, LaunchConfiguration, PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+from launch_ros.actions import Node
+from launch.actions import LogInfo, DeclareLaunchArgument, OpaqueFunction
+
+
+def launch_setup(context, *arg, **args):
+
+    ### DATA INPUT ###
+    vikings_bot_name = LaunchConfiguration("vikings_bot_name").perform(context)
+    x_spawn = LaunchConfiguration('x_spawn').perform(context)
+    y_spawn = LaunchConfiguration('y_spawn').perform(context)
+    yaw_spawn = LaunchConfiguration('yaw_spawn').perform(context)
+
+
+    package_name = "localization_server"
+
+    amcl_config = os.path.join(get_package_share_directory(package_name), "config", f"{vikings_bot_name}_amcl_config.yaml")
+    #c = PathJoinSubstitution([get_package_share_directory(package_name), "config", vikings_bot_name_val, "_amcl_config.yaml"])
+
+    print(x_spawn)
+
+
+    # AMCL node
+    nav2_amcl = Node(
+        namespace=vikings_bot_name,
+        package="nav2_amcl",
+        executable="amcl",
+        name="amcl",
+        output="screen",
+        parameters=[amcl_config,
+                    {"initial_pose": {"x": float(x_spawn)}},
+                    {"initial_pose": {"y": float(y_spawn)}},
+                    ]
+    )
+
+    # Lifecycle node
+    lifecycle_node = Node(namespace=vikings_bot_name,
+                            package='nav2_lifecycle_manager',
+                            executable='lifecycle_manager',
+                            name="amcl_lifecycle_manager",
+                            output='screen',
+                            parameters=[{'use_sim_time': True},
+                                        {'autostart': True},
+                                        {"bond_timeout": 5.0},
+                                        {'node_names': ["amcl"]}
+                            ]
+    )
+
+    return [nav2_amcl, lifecycle_node]
+
+
+def generate_launch_description():
+    # Declare launch arguments
+    vikings_bot_name_arg = DeclareLaunchArgument("vikings_bot_name",
+                    default_value="vikings_bot",
+                    description="Robot name to make it unique")
+    x_spawn_arg = DeclareLaunchArgument('x_spawn', default_value='0.0')
+    y_spawn_arg = DeclareLaunchArgument('y_spawn', default_value='0.0')
+    yaw_spawn_arg = DeclareLaunchArgument('yaw_spawn', default_value='0.0')
+
+    return LaunchDescription([
+        vikings_bot_name_arg,
+        x_spawn_arg,
+        y_spawn_arg,
+        yaw_spawn_arg,
+
+        OpaqueFunction(function=launch_setup)
+    ])
---
+++ launch/test_localization.launch.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    package_name = "localization_server"
+
+    robot_name = "vikings_bot_1"
+    #node_name = robot_name + "_amcl"
+
+    amcl_config = os.path.join(get_package_share_directory(package_name), "config", "test.yaml")
+    #amcl_config = os.path.join(get_package_share_directory('localization_server'), 'config', 'tb3_1_amcl_config.yaml')
+
+    nav2_amcl = Node(
+        #namespace=robot_name,
+        package="nav2_amcl",
+        executable="amcl",
+        name="amcl",
+        output="screen",
+        parameters=[amcl_config]
+    )
+
+    # Lifecycle node
+    lifecycle_node = Node(package='nav2_lifecycle_manager',
+                            executable='lifecycle_manager',
+                            name='nav2_amcl_lifecycle_manager',
+                            output='screen',
+                            parameters=[{'use_sim_time': True},
+                                        {'autostart': True},
+                                        {'node_names': ["amcl"]}
+                            ]
+    )
+    
+    return LaunchDescription([
+        nav2_amcl,
+        lifecycle_node
+    ])
